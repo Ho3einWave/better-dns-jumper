9959b851784d75e1875c9909b59dc8f0d2b5a620
Hosein <48409432+Ho3einWave@users.noreply.github.com>
2025-11-07 17:24:46 +0330

    Add tauri-plugin-single-instance and update NetworkInterfaces styling
    - Introduced `tauri-plugin-single-instance` to ensure only one instance of the application runs.
    - Adjusted the styling of the Network Interfaces component for consistency by removing an unnecessary space in the className attribute.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,6 +18,7 @@ pub struct AppState {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, args, cwd| {}))
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
